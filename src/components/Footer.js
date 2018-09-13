9dffee98f953108d345d1c1f9372a0b4355e2e6f
LeiMing627 <jackperry933jp@gmail.com>
2018-09-13 11:46:00 -0700

    fixed typo in footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer(props){
                     <h5>Links</h5>
                     <ul className="list-unstyled">
                         <li><Link to="/home">Home</Link></li>
-                        <li><Link to="/aboutus">href="#">About</Link></li>
+                        <li><Link to="/aboutus">About</Link></li>
                         <li><Link to="/menu">Menu</Link></li>
                         <li><Link to="/contacus">Contact</Link></li>
                     </ul>
