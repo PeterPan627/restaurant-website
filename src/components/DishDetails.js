693672a2d86d7d032e023ab8ba1db2897c9d01e8
LeiMing627 <jackperry933jp@gmail.com>
2018-09-13 15:55:13 -0700

    added breadcrumb for dishdetail page

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -3,59 +3,71 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 
 
-   function RenderDish({dish}) {
-        if (dish) {
-            return (
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
+function RenderDish({ dish }) {
+    if (dish) {
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+    } else {
+        return (
+            <div></div>
+        )
     }
+}
 
-   function RenderComments({comments}) {
-       return comments.map(comment => {
-                return(
-                <ul className="list-unstyled">
+function RenderComments({ comments }) {
+    return comments.map(comment => {
+        return (
+            <ul className="list-unstyled">
                 <li key={comment.id} >
                     <p>{comment.comment}</p>
-                    <p>{comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'})
-                    .format(new Date(Date.parse(comment.date)))}</p>
+                    <p>{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' })
+                        .format(new Date(Date.parse(comment.date)))}</p>
                 </li>
-                </ul>
-                )
-
-        })
-    }
-
-   const DishDetail = (props) => {
-        const selected = props.dish;
-        return (
-            <div className="container">
-                {selected &&                 
-                        <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        <RenderComments comments={props.comments} />
-                        </div>
-                        </div>
-                }
-
-            </div>
+            </ul>
         )
 
-    }
+    })
+}
+
+const DishDetail = (props) => {
+    const selected = props.dish;
+    return (
+        <div className="container">
+            {selected &&
+            <div>
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
+                }
+
+        </div>
+    )
+
+}
 
 export default DishDetail
 
