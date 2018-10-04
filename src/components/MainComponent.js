59b16abf8c65f16b7918d298c76299c55ba29b9f
LeiMing627 <jackperry933jp@gmail.com>
2018-10-04 12:46:07 -0700

    moved states and connected the component to redux store

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,29 +6,32 @@ import Header from './Header';
 import Footer from './Footer';
 import About from './About';
 import Contact from './Contact';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+/* will map the Redux Store's state into props that will become available to the component */
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+    // now states are available as props
+}
+
 
 class Main extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    }
   }
   
   render() {
     const HomePage = () => {
       return(
-        <Home dish={this.state.dishes.filter(dish => dish.featured)[0]} 
-              promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-              leader={this.state.leaders.filter(leader => leader.featured)[0]}
+        <Home dish={this.props.dishes.filter(dish => dish.featured)[0]} 
+              promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+              leader={this.props.leaders.filter(leader => leader.featured)[0]}
         />
       );
     }
@@ -36,8 +39,8 @@ class Main extends Component {
     const DishWithId = ({match}) => {
       return(
         /* Convert url's disId to a base 10 integer value */
-        <DishDetail dish={this.state.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
-          comments={this.state.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
+        <DishDetail dish={this.props.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
           />
         );
     }
@@ -49,11 +52,11 @@ class Main extends Component {
           {/* Simply call the component that doesn't need any props */}
           <Route path="/home" component={HomePage} />
           {/* Passing props to the componenet in <Route> */}
-          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
+          <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} /> } />
           {/* dishId is the key */}
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
-          <Route exact path="/aboutus" component={() => <About leaders ={this.state.leaders} />} />
+          <Route exact path="/aboutus" component={() => <About leaders ={this.props.leaders} />} />
           <Redirect to="/home" /> {/* Anyythin doesn't match other Routers */}
         </Switch>
         <Footer />
@@ -62,4 +65,6 @@ class Main extends Component {
   }
 }
 
-export default Main;
+// Connecting the component to the Redux Store
+// without withRouter it doesn't work
+export default withRouter(connect(mapStateToProps)(Main));
