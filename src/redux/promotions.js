41731f4739bf5bfcfa94ff4a106a210d0abd3228
LeiMing627 <jackperry933jp@gmail.com>
2018-12-03 14:42:18 -0800

    combined reducers

---
+++ src/redux/promotions.js
@@ -0,0 +1,8 @@
+import { PROMOTIONS } from '../shared/promotions';
+
+export const Promitions = (state = PROMOTIONS, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
