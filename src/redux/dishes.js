382e2fd075ffb74b004938daa3de45d25093fbaa
LeiMing627 <jackperry933jp@gmail.com>
2018-12-06 14:31:29 -0800

    fixed typo

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,6 +1,22 @@
-export const Dishes = (state = DISHES, action) => {
+import * as ActionTypes from './ActionTypes';
+
+export const Dishes = (state = {
+        isLoading: true, 
+        error: null,
+        dishes: []
+    }, action) => {
     switch(action.type) {
-        default: //default state
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: dalse, error: null, dishes: action.payload}
+        
+            case ActionTypes.DISHES_LOADING:
+            // take the state and create a new object, than modify the copied one
+            return {...state, isLoading: true, error: null, dishes: []}
+
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, error: action.payload, dishes: []}
+
+        default:
             return state; 
     }
 }
