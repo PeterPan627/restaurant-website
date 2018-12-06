44e02cae354b7f7fb13ed2e1d86c4f8d294a9c0c
LeiMing627 <jackperry933jp@gmail.com>
2018-12-06 10:40:29 -0800

    completed Comment Reducuer

--- src/redux/comment.js
+++
@@ -1,8 +0,0 @@
-import { COMMENTS } from '../shared/comments';
-
-export const Comments = (state = COMMENTS, action) => {
-    switch(action.type) {
-        default:
-            return state;
-    }
-}
