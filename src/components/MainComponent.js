72864649273f37555b8521848ba774471424591c
LeiMing627 <jackperry933jp@gmail.com>
2018-12-06 14:45:34 -0800

    added fetchDishes thunk and modified Homepage and Dish Detail pages' props

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './About';
 import Contact from './Contact';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import  { addComment } from '../redux/ActionCreators';
+import  { addComment, fetchDishes } from '../redux/ActionCreators';
 
 /* will map the Redux Store's state into props that will become available to the component */
 const mapStateToProps = state => {
@@ -21,18 +21,25 @@ const mapStateToProps = state => {
     // now states are available as props
 }
 
+//addCommnet action creator and fetchDishes thunk wll be available in the main component
 const mapDispatchToProps = dispatch => ({
   // The object that is returned from addComment action creator will be given as a parameter to dispatch
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {dispatch(fetchDishes())}
 })
 
 class Main extends Component {
 
-  
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
     const HomePage = () => {
       return(
-        <Home dish={this.props.dishes.filter(dish => dish.featured)[0]} 
+        <Home dish={this.props.dishes.dishes.filter(dish => dish.featured)[0]} 
+              dishesLoading={this.props.dishes.isLoading}
+              dishesError={this.props.dishes.error}
               promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
               leader={this.props.leaders.filter(leader => leader.featured)[0]}
         />
@@ -42,7 +49,9 @@ class Main extends Component {
     const DishWithId = ({match}) => {
       return(
         /* Convert url's disId to a base 10 integer value */
-        <DishDetail dish={this.props.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+        <DishDetail dish={this.props.dishes.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+          isLoading={this.props.dishes.isLoading}
+          error={this.props.dishes.error}
           comments={this.props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
           addComment={this.props.addComment}
           />
