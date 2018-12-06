82d804e8d14b88728627890d685b5a2fb75deebe
LeiMing627 <jackperry933jp@gmail.com>
2018-12-06 10:38:21 -0800

    added Action Creators

---
+++ src/redux/ActionCreators.js
@@ -0,0 +1,12 @@
+import * as ActionTypes from './ActionTypes';
+
+// addComment Action Creator
+export const addComment = (dishId, rating, author, comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment
+    }
+});
