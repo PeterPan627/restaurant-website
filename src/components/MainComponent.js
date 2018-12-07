0bd1ac2cf54851090f3f3326676ce6864d4c5195
LeiMing627 <jackperry933jp@gmail.com>
2018-12-06 18:08:21 -0800

    converted localform to react-redux-form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Contact from './Contact';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import  { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 /* will map the Redux Store's state into props that will become available to the component */
 const mapStateToProps = state => {
@@ -25,7 +26,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   // The object that is returned from addComment action creator will be given as a parameter to dispatch
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 })
 
 class Main extends Component {
@@ -68,7 +70,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} /> } />
           {/* dishId is the key */}
           <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={() => <Contact setFeedbackForm={this.props.resetFeedbackForm} />} />
           <Route exact path="/aboutus" component={() => <About leaders ={this.props.leaders} />} />
           <Redirect to="/home" /> {/* Anyythin doesn't match other Routers */}
         </Switch>
