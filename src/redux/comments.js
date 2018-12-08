71ad53621aac3ea33f5c6edf217533be84a925e4
LeiMing627 <jackperry933jp@gmail.com>
2018-12-08 15:16:21 -0800

    updated comment reducer

---
+++ src/redux/comments.js
@@ -0,0 +1,26 @@
+import { COMMENTS } from '../shared/comments';
+import * as ActionTypes from './ActionTypes';
+
+export const Comments = (state = {
+        error: null,
+        comments: []
+}, action) => {
+    switch(action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, isLoading: false, error: null, comments: action.payload}
+        
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, isLoading: false, error: action.payload, comments: []}
+
+        case ActionTypes.ADD_COMMENT:
+            var comment = action.payload;
+            comment.id = state.comments.length;
+            comment.date = new Date().toISOString();
+            //Test:
+            console.log("Comment:", comment)
+            // Does NOT mutate the state
+            return{...state, comments: state.comments.concat(comment)}; // Add comment into set of comments and return the new comment
+        default:
+            return state;
+    }
+}
