a64f8384b402767bf8f08404fe7f6d82b5ec6186
LeiMing627 <jackperry933jp@gmail.com>
2018-12-08 15:21:13 -0800

    updated promotion reducer with add, loading and failed functions

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,7 +1,21 @@
 import { PROMOTIONS } from '../shared/promotions';
 
-export const Promitions = (state = PROMOTIONS, action) => {
+export const Promotions = (state = {
+        isLoading: true,
+        error: null,
+        promotions: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_PROMOS:
+            return {...state, isLoading: false, error: null, promos: action.payload}
+    
+        case ActionTypes.PROMOS_LOADING:
+            // take the state and create a new object, than modify the copied one
+            return {...state, isLoading: true, error: null, promos: []}
+
+        case ActionTypes.PROMOS_FAILED:
+            return {...state, isLoading: false, error: action.payload, promotions: []}
+
         default:
             return state;
     }
